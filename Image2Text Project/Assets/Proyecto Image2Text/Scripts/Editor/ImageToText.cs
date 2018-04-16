997454f455ad55dd2506a3420969e103804dff5d
Moon Antonio <pinchodev91@gmail.com>
2018-04-16 15:48:47 +0200

    Evento OnEnable que obtiene los valores necesarios

--- Image2Text Project/Assets/Proyecto Image2Text/Scripts/Editor/ImageToText.cs
+++ Image2Text Project/Assets/Proyecto Image2Text/Scripts/Editor/ImageToText.cs
@@ -19,6 +19,24 @@ namespace MoonAntonio
 	/// </summary>
 	public class ImageToText : EditorWindow
 	{
+		#region Constantes
+		/// <summary>
+		/// <para>Key que contiene la ultima carpeta contenedora.</para>
+		/// </summary>
+		public const string carpetaContenedoraKey = "ImageToTextCCK";
+		#endregion
+
+		#region Variables Privadas
+		/// <summary>
+		/// <para>Direccion de la carpeta contenedora.</para>
+		/// </summary>
+		private string carpetaContenedora;
+		/// <summary>
+		/// <para>Contiene la preview de la imagen.</para>
+		/// </summary>
+		private Rect preImagen;
+		#endregion
+
 		#region Menu
 		/// <summary>
 		/// <para>Menu de <see cref="ImageToText"/>.</para>
@@ -26,7 +44,22 @@ namespace MoonAntonio
 		[MenuItem("MoonAntonio/Image2Text")]
 		public static void Window()
 		{
+			// Crear window
 			GetWindow(typeof(ImageToText), true, "Image2Text");
+		}
+		#endregion
+
+		#region Eventos
+		/// <summary>
+		/// <para>Al habilitarse <see cref="ImageToText"/>.</para>
+		/// </summary>
+		public void OnEnable()
+		{
+			// Obtener valores
+			minSize = new Vector2(300, 93);
+			maxSize = new Vector2(4000, 93);
+			carpetaContenedora = EditorPrefs.GetString(carpetaContenedoraKey);
+			preImagen = new Rect(0, 2, 84, 84);
 		}
 		#endregion
 
