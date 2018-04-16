f89d86a385809c968e4ea67d8114436681785448
Moon Antonio <pinchodev91@gmail.com>
2018-04-16 16:00:16 +0200

    Update GUI

--- Image2Text Project/Assets/Proyecto Image2Text/Scripts/Editor/ImageToText.cs
+++ Image2Text Project/Assets/Proyecto Image2Text/Scripts/Editor/ImageToText.cs
@@ -10,6 +10,7 @@
 #region Librerias
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 #endregion
 
 namespace MoonAntonio
@@ -27,14 +28,18 @@ namespace MoonAntonio
 		#endregion
 
 		#region Variables Privadas
+		private string nombreArchivo;
+		private string nombreArchivoOri;
 		/// <summary>
 		/// <para>Direccion de la carpeta contenedora.</para>
 		/// </summary>
 		private string carpetaContenedora;
 		/// <summary>
-		/// <para>Contiene la preview de la imagen.</para>
+		/// <para>Contiene la dimension de la preview de la imagen.</para>
 		/// </summary>
-		private Rect preImagen;
+		private Rect preImagenRect;
+		private byte[] data;
+		private Texture2D preImagen;
 		#endregion
 
 		#region Menu
@@ -59,7 +64,84 @@ namespace MoonAntonio
 			minSize = new Vector2(300, 93);
 			maxSize = new Vector2(4000, 93);
 			carpetaContenedora = EditorPrefs.GetString(carpetaContenedoraKey);
-			preImagen = new Rect(0, 2, 84, 84);
+			preImagenRect = new Rect(0, 2, 84, 84);
+		}
+		#endregion
+
+		#region GUI
+		/// <summary>
+		/// <para>GUI</para>
+		/// </summary>
+		public void OnGUI()
+		{
+			EditorGUILayout.BeginHorizontal();
+			{
+				var controlsRect = EditorGUILayout.BeginVertical(GUILayout.Width(Screen.width - 98));
+				{
+					EditorGUILayout.Space();
+					EditorGUILayout.BeginHorizontal();
+					{
+						EditorGUILayout.LabelField("Nombre", GUILayout.Width(80));
+						nombreArchivo = EditorGUILayout.TextField(nombreArchivo);
+					}
+					EditorGUILayout.EndHorizontal();
+
+					EditorGUILayout.BeginHorizontal();
+					{
+						EditorGUILayout.LabelField("Carpeta", GUILayout.Width(80));
+						EditorGUILayout.TextField(carpetaContenedora);
+						if (GUILayout.Button("Select"))
+						{
+							carpetaContenedora = EditorUtility.OpenFolderPanel("Seleccionar carpeta de salida", carpetaContenedora, "");
+							EditorPrefs.SetString(carpetaContenedoraKey, carpetaContenedora);
+						}
+					}
+					EditorGUILayout.EndHorizontal();
+
+					EditorGUILayout.BeginHorizontal();
+					{
+						EditorGUILayout.LabelField("Archivo", GUILayout.Width(80));
+						EditorGUILayout.TextField(nombreArchivoOri);
+						if (GUILayout.Button("Select"))
+						{
+							nombreArchivoOri = EditorUtility.OpenFilePanelWithFilters("Seleccionar imagen", "Assets/",
+								new string[] { "Image files", "png,jpg,jpeg,bmp,dds,gif,psd,tga,tiff" });
+							if (File.Exists(nombreArchivoOri))
+							{
+								data = File.ReadAllBytes(nombreArchivoOri);
+
+								if (preImagen == null)
+								{
+									preImagen = new Texture2D(2, 2);
+								}
+								preImagen.LoadImage(data);
+							}
+						}
+					}
+					EditorGUILayout.EndHorizontal();
+
+					if (GUILayout.Button("Convertir"))
+					{
+						if (string.IsNullOrEmpty(nombreArchivo))
+						{
+							EditorUtility.DisplayDialog("Error", "Falta el nombre del archivo!", "OK");
+						}
+						else if (string.IsNullOrEmpty(nombreArchivoOri))
+						{
+							EditorUtility.DisplayDialog("Error", "Falta la imagen!", "OK");
+						}
+						else
+						{
+							// Guardar Textura
+						}
+					}
+				}
+				EditorGUILayout.EndVertical();
+
+				preImagenRect.x = controlsRect.xMax + 4;
+				GUI.DrawTexture(preImagenRect, preImagen);
+			}
+			EditorGUILayout.EndHorizontal();
 		}
 		#endregion
 
